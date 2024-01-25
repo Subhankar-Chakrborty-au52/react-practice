3508d0e1c9b1d403af58b9cde483c16a99ac9827
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-01-25 14:51:18 +0530

    login ui complete

---
+++ src/components/Logout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Logout.css";
+
+const Logout = () => {
+  return;
+  <>
+    <div>
+      <h1>
+        Welcome <span className="user_name">Raja</span>
+        <button className="logout_button">Logout</button>
+      </h1>
+    </div>
+  </>;
+};
+
+export default Logout;
